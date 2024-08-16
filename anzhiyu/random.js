0019460fda91329b6341383338e4a175ce89a7fe
zhongzl728 <923172051@qq.com>
2024-08-17 00:05:09 +0800

    Site updated: 2024-08-17 00:05:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/3eeb.html","posts/e705.html","posts/ca5d.html","posts/a62c.html","posts/3c9d.html"];function toRandomPost(){
+var posts=["posts/3eeb.html","posts/a62c.html","posts/3c9d.html","posts/ca5d.html","posts/e705.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
