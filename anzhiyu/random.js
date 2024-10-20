8382a8e4c67af9ea707c996aab0c65d2db56796b
zilong <923172051@qq.com>
2024-10-20 14:23:55 +0800

    Site updated: 2024-10-20 14:23:55

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/3eeb.html","posts/2135.html","posts/e705.html","posts/a62c.html","posts/ca5d.html","posts/3c9d.html"];function toRandomPost(){
+var posts=["posts/2135.html","posts/3c9d.html","posts/ca5d.html","posts/3eeb.html","posts/e705.html","posts/a62c.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
